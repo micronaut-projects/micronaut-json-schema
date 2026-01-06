b4cfe35cd2b13eea62ddc8ae9d8a677862a59fa4
issam liqali <liqaliissam@gmail.com>
2026-01-06 14:35:57 +0100

    match Jackson 3's alphabetical property ordering

--- json-schema-common/src/test/groovy/io/micronaut/jsonschema/serialization/SchemaSerializationSpec.groovy
+++ json-schema-common/src/test/groovy/io/micronaut/jsonschema/serialization/SchemaSerializationSpec.groovy
@@ -79,8 +79,8 @@ class SchemaSerializationSpec extends Specification {
 
         where:
         schema                                             | expectedJson
-        Schema.number().setMinimum(12).setMaximum(13)      | '{"type":"number","maximum":13,"minimum":12}'
-        Schema.object().putProperty("a", Schema.integer()) | '{"type":"object","properties":{"a":{"type":"integer"}}}'
+        Schema.number().setMinimum(12).setMaximum(13)      | '{"maximum":13,"minimum":12,"type":"number"}'
+        Schema.object().putProperty("a", Schema.integer()) | '{"properties":{"a":{"type":"integer"}},"type":"object"}'
     }
 
     void "test #expectedJson serialization"() {
