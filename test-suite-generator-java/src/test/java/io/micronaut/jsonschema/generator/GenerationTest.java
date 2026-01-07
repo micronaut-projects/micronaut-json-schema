00f16cea3daccbf45dc64e65b517fcdc8c7aa978
issam liqali <liqaliissam@gmail.com>
2026-01-07 17:30:44 +0100

    fix build

--- test-suite-generator-java/src/test/java/io/micronaut/jsonschema/generator/GenerationTest.java
+++ test-suite-generator-java/src/test/java/io/micronaut/jsonschema/generator/GenerationTest.java
@@ -11,9 +11,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class GenerationTest {
     @Test
