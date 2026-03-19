e99ea3ccdacfaa2acda0076290476cb037638e92
issam liqali <liqaliissam@gmail.com>
2026-03-19 15:30:54 +0000

    correct is error.toString()

--- json-schema-validation/src/main/java/io/micronaut/jsonschema/validation/ValidationMessageAdapter.java
+++ json-schema-validation/src/main/java/io/micronaut/jsonschema/validation/ValidationMessageAdapter.java
@@ -39,7 +39,7 @@ public class ValidationMessageAdapter implements ValidationMessage {
     @Override
     @NonNull
     public String getMessage() {
-        return error.getMessage();
+        return error.toString();
     }
 
     /**
